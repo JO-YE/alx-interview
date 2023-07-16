1cb4c1d204911bf4c7d191c4e7480ce4eccc7b1e
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-07-16 21:00:11 +0100

    new method

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,22 +9,17 @@ def minOperations(n):
         return an integar
         if `n` is impossible to achieve, return 0
     '''
-    # If n is less than or equal to 1, no operations are needed
     if n <= 1:
         return 0
-    # Starting with a large value to keep track of the minimum operations
-    min_ops = float('inf')
 
-    # Looping through all possible divisors of n
-    for divisor in range(2, n + 1):
-        # Checking if the current divisor divides `n` evenly
+    num_operations = 0
+    divisor = 2
+
+    while n > 1:
         if n % divisor == 0:
-            # Calculate the number of operations needed for this divisor
-            num_operations = divisor + minOperations(n // divisor)
+            n //= divisor
+            num_operations += divisor
+        else:
+            divisor += 1
 
-            # Updating the minimum number of operations if the current
-            # divisor requires fewer operations
-            min_ops = min(min_ops, num_operations)
-
-    # Return the minimum number of operations needed for n
-    return min_ops
+    return num_operations
