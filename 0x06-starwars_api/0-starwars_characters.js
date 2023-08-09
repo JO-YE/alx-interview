437b4da7f1333edab492b446f91110b48dbb70b5
JO-YE <ainarachealoluwadamilola@gmail.com>
2023-08-09 20:30:38 +0100

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,11 +2,11 @@
 // A script that prints all characters of a Star Wars movie
 
 // Importing the 'request' module
-const request = require('request');
+const request = require("request");
 
 /* checking if the correct number of arguments is provided */
 if (process.argv.length !== 3) {
-  console.log('Kindly provide your movie ID');
+  console.log("Kindly provide your movie ID");
   process.exit(1);
 }
 
@@ -19,11 +19,11 @@ const url = `https://swapi.dev/api/films/${movieId}/`;
 /* Performing the request to the movie's API endpoint */
 request(url, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
     return;
   }
   if (response.statusCode !== 200) {
-    console.error('API Error:', body);
+    console.error("API Error:", body);
     return;
   }
   /* Parsing  the JSON response */
@@ -34,36 +34,33 @@ request(url, (error, response, body) => {
   fetchAndDisplayCharacters(characters);
 });
 
- /* Function to fetch and display character names */
-  function fetchAndDisplayCharacters(characters) {
-    const Promises = characters.map(individualUrl) => {
-      request(individualUrl, (error, response, body) => {
-        if(error) {
-          reject(error);
-          return;
-	}
+/* Function to fetch and display character names */
+function fetchAndDisplayCharacters(characters) {
+  const Promises = characters.map((individualUrl) => {
+    request(individualUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+        return;
+      }
 
-        if (response.statusCode !== 200) {
-          reject('API Error for ${individualUrl}')
-          return;
-	}
+      if (response.statusCode !== 200) {
+        reject("API Error for ${individualUrl}");
+        return;
+      }
 
-        const characterData = JSON.parse(body);
-        resolve(characterData.name);
-      });
+      const characterData = JSON.parse(body);
+      resolve(characterData.name);
     });
-  };
+  });
 
   Promise.all(Promises)
-    .then(characterNames => {
+    .then((characterNames) => {
       /* Displaying character names*/
-      characterNames.forEach(name => {
+      characterNames.forEach((name) => {
         console.log(name);
       });
     })
-    .catch(error => {
-      console.error('Error fetching character data:', error);
+    .catch((error) => {
+      console.error("Error fetching character data:", error);
     });
-  
-
-
+}
