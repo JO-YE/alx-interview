a51365dea572ceea57a3492e0877d5faa1b06592
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-08-09 20:47:08 +0100

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,15 +2,15 @@
 // A script that prints all characters of a Star Wars movie
 
 // Importing the 'request' module
-const request = require("request");
+const request = require('request');
 
 /* checking if the correct number of arguments is provided */
 if (process.argv.length !== 3) {
-  console.log("Kindly provide your movie ID");
+  console.log('Kindly provide your movie ID');
   process.exit(1);
 }
 
-/*Extracting the Movie ID from command line arguments */
+/* Extracting the Movie ID from command line arguments */
 const movieId = process.argv[2];
 
 /* Constructing the URL for the movie's API endpoint */
@@ -19,11 +19,11 @@ const url = `https://swapi.dev/api/films/${movieId}/`;
 /* Performing the request to the movie's API endpoint */
 request(url, (error, response, body) => {
   if (error) {
-    console.error("Error:", error);
+    console.error('Error:', error);
     return;
   }
   if (response.statusCode !== 200) {
-    console.error("API Error:", body);
+    console.error('API Error:', body);
     return;
   }
   /* Parsing  the JSON response */
@@ -35,32 +35,34 @@ request(url, (error, response, body) => {
 });
 
 /* Function to fetch and display character names */
-function fetchAndDisplayCharacters(characters) {
-  const Promises = characters.map((individualUrl) => {
-    request(individualUrl, (error, response, body) => {
-      if (error) {
-        reject(error);
-        return;
-      }
+function fetchAndDisplayCharacters (characters) {
+  const Promises = characters.map(individualUrl => {
+    return new Promise((resolve, reject) => {
+      request(individualUrl, (error, response, body) => {
+        if (error) {
+          reject(error);
+          return;
+        }
 
-      if (response.statusCode !== 200) {
-        reject("API Error for ${individualUrl}");
-        return;
-      }
+        if (response.statusCode !== 200) {
+          reject('API Error for ${individualUrl}');
+          return;
+        }
 
-      const characterData = JSON.parse(body);
-      resolve(characterData.name);
+        const characterData = JSON.parse(body);
+        resolve(characterData.name);
+      });
     });
   });
 
   Promise.all(Promises)
     .then((characterNames) => {
-      /* Displaying character names*/
+      /* Displaying character names */
       characterNames.forEach((name) => {
         console.log(name);
       });
     })
     .catch((error) => {
-      console.error("Error fetching character data:", error);
+      console.error('Error fetching character data:', error);
     });
 }
